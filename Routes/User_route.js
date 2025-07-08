7c139671e3ebec6b3c906c97836a5f5bc037547b
comfort-parker <comfortparker2001@outlook.com>
2025-07-08 18:12:26 +0000

    first commit

---
+++ Routes/User_route.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import { 
+  register,
+  verifyOTP,
+  login,
+  forgotPassword,
+  resetPassword, getUserProfile, updateUserProfile, deleteUserAccount}  from '../Controllers/UserAuthentication.js';
+
+  import { protect, adminOnly } from '../Middleware/authen_users.js';
+const userRoute = express.Router();
+
+// @route   POST /api/auth/register
+userRoute.post('/register', register);
+
+// @route   POST /api/auth/verify-otp
+userRoute.post('/verify-otp', verifyOTP);
+
+// @route   POST /api/auth/login
+userRoute.post('/login', login);
+
+// @route   POST /api/auth/forgot-password
+userRoute.post('/forgot-password', forgotPassword);
+
+// @route   POST /api/auth/reset-password/:token
+userRoute.post('/reset-password/:token', resetPassword);
+
+userRoute.get('/profile', protect, getUserProfile);
+userRoute.put('/profile', protect, updateUserProfile);
+userRoute.delete('/profile', protect, deleteUserAccount);
+
+export default userRoute;
