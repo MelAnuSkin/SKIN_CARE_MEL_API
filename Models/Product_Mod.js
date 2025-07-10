a1281a5f4df3fcdbb6c8186edfc00f4ada3c11f9
comfort-parker <comfortparker2001@outlook.com>
2025-07-10 16:57:35 +0000

    Product management files have been added

---
+++ Models/Product_Mod.js
@@ -0,0 +1,55 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Product name is required'],
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: [true, 'Product description is required'],
+    },
+    price: {
+      type: Number,
+      required: [true, 'Product price is required'],
+      min: 0,
+    },
+    category: {
+      type: String,
+      enum: [
+        'Cleanser',
+        'Toner',
+        'Serum',
+        'Moisturizer',
+        'Sunscreen',
+        'Treatment',
+        'Mask',
+        'Exfoliant'
+      ],
+      index: true,
+      required: [true, 'Product category is required'],
+    },
+    image: {
+      type: String,
+      default: '',
+    },
+    stock: {
+      type: Number,
+      required: true,
+      min: 0,
+      default: 0,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Product = mongoose.model('Product', productSchema);
---
+++ Routes/Product_routes.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import { protect, adminOnly } from '../Middleware/authen_users.js';
+import { parser } from '../Middleware/Upload.js';
+import { createProduct, getProductById, getProducts, updateProduct, deleteProduct, searchProducts } from '../Controllers/Product_Con.js';
+
+const productRoute = express.Router();
+
+// Public routes
+productRoute.get('/', getProducts);
+productRoute.get('/:id', getProductById);
+productRoute.get('/search', searchProducts)
+
+// Admin-only routes
+productRoute.post('/', protect, adminOnly, parser.single('image'), createProduct);
+productRoute.put('/:id', protect, adminOnly, parser.single('image'), updateProduct);
+productRoute.delete('/:id', protect, adminOnly, deleteProduct);
+
+export default productRoute;
