a2052cd029b149054c8d855d2f76aa3251c02015
comfort-parker <comfortparker2001@outlook.com>
2025-08-01 16:39:23 +0000

    mc

--- Models/Contact_Mod.js
+++ Models/Contact_Mod.js
@@ -1,11 +1,18 @@
 import mongoose from 'mongoose';
 
 const contactSchema = new mongoose.Schema({
-  fullName: { type: String, required: true },
-  email:    { type: String, required: true },
+  fullName:    { type: String, required: true },
+  email:       { type: String, required: true },
   inquiryType: { type: String, required: true },
-  subject:  { type: String, required: true },
-  message:  { type: String, required: true },
+  subject:     { type: String, required: true },
+  message:     { type: String, required: true },
+  adminReply: {
+    type: String,
+    default: null 
+  },
+  repliedAt: {
+    type: Date
+  },
   createdAt: { type: Date, default: Date.now }
 });
 
